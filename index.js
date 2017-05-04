4e7af81fc188b6bf9c703ef8fa03e9bc0899160b
Adrian Biedrzycki <a@modules.io>
2017-05-04 11:40:07 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const fs = require('fs');
+
+const mkdirp = require('mkdirp');
+
+class Crapdb {
+  constructor({path = 'data.json'} = {}) {
+    this.path = path;
+
+    this._data = {};
+
+    this.set = _debounce(this.set.bind(this));
+  }
+
+  get() {
+    return this._data;
+  }
+
+  set(next) {
+    mkdirp(path.basename(this.path), err => {
+      if (!err) {
+        fs.writeFile(this.path, JSON.stringify(this._data, null, 2), err => {
+          if (err) {
+            console.warn(err);
+          }
+
+          next();
+        });
+      } else {
+        console.warn(err);
+
+        next();
+      }
+    });
+  }
+}
+
+const _debounce = fn => {
+  let running = false;
+  let queued = false;
+
+  const _go = () => {
+    if (!running) {
+      running = true;
+
+      fn(() => {
+        running = false;
+
+        if (queued) {
+          queued = false;
+
+          _go();
+        }
+      });
+    } else {
+      queued = true;
+    }
+  };
+  return _go;
+};
+
+module.exports = Crapdb;
